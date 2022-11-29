146af8c2064580760b702fae30b5e573f5208ebe
Beniamin <106469071+redve-dev@users.noreply.github.com>
2022-11-29 20:56:34 +0100

    refactor

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import cv2
 import numpy as np
 
-def main():
-    input_img = cv2.imread('pokeball.png')
-    max_x, max_y, _ = input_img.shape
-    d=22
+def convert_image_to_array(image, d):
+    max_x, max_y, _ = image.shape
     dx, dy = d, d
-    output_img = np.array( [[0 for _ in range(int(max_x/dy))] for _ in range(int(max_y/dy))] )
+    # empty array of size image_size / delta
+    output_img = np.array( [[0]*int(max_x/dx) for _ in range(int(max_y/dy))] )
     for x in range(0, max_x+1, dx):
         for y in range(0, max_y+1, dy):
-            avg = np.average(input_img[x-1, y-1])
+            avg = np.average(image[x-1, y-1])
             output_img[int(x/dx)-1][int(y/dy)-1]=avg
-
     output_img.transpose()
+    return output_img
 
+def main():
+    input_img = cv2.imread('pokeball.png')
+    output_img = convert_image_to_array(input_img, 22)
     for row in output_img:
         print(row)
 
