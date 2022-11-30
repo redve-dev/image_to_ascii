97424489e0d134b4f7d8dea950b5c950fffe5b77
Beniamin <106469071+redve-dev@users.noreply.github.com>
2022-11-30 16:32:02 +0100

    refactor

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 import cv2
 import numpy as np
+import math
+
+def rgb_to_gray(r, g, b):
+    return np.average([r, g, b])
 
 def convert_image_to_array(image, d):
-    max_x, max_y, _ = image.shape
+    img_width, img_height, _ = image.shape
     # separate dx, dy in case i want change "scaling" of output image
     dx, dy = d, d
     # empty array of size image_size / delta
-    output_img = np.array( [[0]*int(max_x/dx) for _ in range(int(max_y/dy))] )
-    for x in range(0, max_x+1, dx):
-        for y in range(0, max_y+1, dy):
-            avg = np.average(image[x-1, y-1])
-            output_img[int(x/dx)-1][int(y/dy)-1]=avg
-    output_img.transpose()
+    output_img = [[0] * math.ceil(img_width/dx) ] * math.ceil(img_height/dy)
+    s = set()
+    for y, row in enumerate(output_img):
+        for x in range(len(row)):
+            avg = rgb_to_gray(*image[x*dx, y*dy])
+            output_img[y][x]=math.floor(avg)
+            s.add(output_img[y][x])
+
+    print(s)
     return output_img
 
 def main():
     input_img = cv2.imread('pokeball.png')
+    # input_img = cv2.imread('fox.jpg')
     output_img = convert_image_to_array(input_img, 22)
     for row in output_img:
         print(row)
