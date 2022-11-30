a5497fcb489c921dcf19fedd73ddee9956b61666
Beniamin <106469071+redve-dev@users.noreply.github.com>
2022-11-30 17:33:02 +0100

    refactor

--- main.py
+++ main.py
@@ -1,29 +1,35 @@
 import cv2
 import numpy as np
-import math
 
 def rgb_to_gray(r, g, b):
     return np.average([r, g, b])
+
+def map_gray_to_char(val):
+    t = val / 255
+    density = 'Ñ@#W$9876543210?!abc;:+=-,._ '
+    index = int(len(density)*t)
+    return density[index-1]
 
 def convert_image_to_array(image, d):
     img_width, img_height, _ = image.shape
     # separate dx, dy in case i want change "scaling" of output image
     dx, dy = d, d
     # empty array of size image_size / delta
-    output_img = [[0 for _ in range(math.floor(img_width/dx))]  for _ in range(math.floor(img_height/dy))]
-    for y, row in enumerate(output_img):
-        for x in range(len(row)):
+    output_img = [['0' for _ in range(int(img_width/dx))]  for _ in range(int(img_height/dy))]
+    for y in range(len(output_img)):
+        for x in range(len(output_img[y])):
             avg = rgb_to_gray(*image[x*dx, y*dx])
-            output_img[y][x]=math.floor(avg)
+            char = map_gray_to_char(avg)
+            output_img[y][x]=char
 
-    return output_img
+    return np.array(output_img).transpose()
 
 def main():
-    # input_img = cv2.imread('pokeball.png')
     input_img = cv2.imread('fox.jpg')
-    output_img = convert_image_to_array(input_img, 22)
+    output_img = convert_image_to_array(input_img, 5)
     for row in output_img:
-        print(row)
+        output = "".join(char for char in row)
+        print(output)
 
 if __name__ == "__main__":
     main()
